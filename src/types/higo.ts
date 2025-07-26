2aaff12ad68952a75d437583e4ab7cda49dbbafe
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-07-26 08:56:55 +0700

    create type higo

---
+++ src/types/higo.ts
@@ -0,0 +1,10 @@
+export interface HigoData {
+  id: string;
+  Name: string;
+  Age: number;
+  gender: string;
+  Email: string;
+  Digital_Interest: string;
+  Location_Type: string;
+  Number: number;
+}
