41bb1a0a6695fa77ac1b508086536ad18d15ce71
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-07-26 10:22:20 +0700

    fixing deployment

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4999/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 export default api;
